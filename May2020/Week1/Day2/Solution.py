8fb4386d5ba0c0804ae044e5368285a857c1e0e9
Dhanabal <43275764+DhanabalShanmugam@users.noreply.github.com>
2020-05-04 11:52:46 +0530

    Create Solution.py

---
+++ May2020/Week1/Day2/Solution.py
@@ -0,0 +1,18 @@
+# Add the all the elements of jewels in set.
+# Iter through stone 
+     #if that stone present in set increase count
+
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        jewels = set()
+        count = 0
+        
+        for i in J:
+            jewels.add(i)
+        
+        for i in S:
+            if i in jewels:
+                count +=1
+        return count
+        
+        
