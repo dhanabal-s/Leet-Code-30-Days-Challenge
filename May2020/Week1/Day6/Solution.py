d25e4f9c0c35186c945d7466496c228288bcbe97
Dhanabal S <43275764+DhanabalShanmugam@users.noreply.github.com>
2020-05-06 13:45:55 +0530

    Create Solution.py

---
+++ May2020/Week1/Day6/Solution.py
@@ -0,0 +1,31 @@
+# We solve the problem in two methods
+
+# Method-1
+# Sort the give list, return the middle element of the list. Because majority element always greater [n/2].
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        nums.sort()
+        return nums[int(len(nums)/2)]
+        
+Time Complexity : O(NlogN)
+Space Complexity : O(1)
+
+# Method-2
+# Count no.of occurence of each element in list using dict
+# return the element if its count is greater than [n/2]
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        eleCount = {}
+        for ele in nums:
+            eleCount[ele] = eleCount.get(ele,0)+1
+            if(eleCount[ele] > int(len(nums)/2)):
+                return ele
+
+Time Complexity : O(N)
+Space Complexity : O(N)
+
+Note: if you apply both methods in a leetcode, method-1 beats more than 98% soln, but method-2 beats only a 30% soln. 
+      Even method-2 big notaion smaller than method-1. So, everyone is better than another in different situations.
+      real life also like that so, belive youself, love youself 
