b3fbb55ef26b40e2c8735360eca5d92f89e70253
Dhanabal S <43275764+DhanabalShanmugam@users.noreply.github.com>
2020-05-17 14:18:46 +0530

    Create Problem.py

---
+++ May2020/Week3/Day_17/Problem.py
@@ -0,0 +1,29 @@
+# Problem
+ __Find All Anagrams in a String__
+ 
+Given a string s and a non-empty string p, find all the start indices of p's anagrams in s.</br>
+Strings consists of lowercase English letters only and the length of both strings s and p will not be larger than 20,100.</br>
+The order of output does not matter.</br>
+
+Example 1:</br>
+Input:</br>
+s: "cbaebabacd" p: "abc"</br>
+Output:</br>
+[0, 6]</br>
+Explanation:</br>
+The substring with start index = 0 is "cba", which is an anagram of "abc".</br>
+The substring with start index = 6 is "bac", which is an anagram of "abc".</br>
+
+Example 2:</br>
+Input:</br>
+s: "abab" p: "ab"</br>
+Output:</br>
+[0, 1, 2]</br>
+Explanation:</br>
+The substring with start index = 0 is "ab", which is an anagram of "ab".</br>
+The substring with start index = 1 is "ba", which is an anagram of "ab".</br>
+The substring with start index = 2 is "ab", which is an anagram of "ab".</br>
+
+[LeetCode Problem link](https://leetcode.com/explore/featured/card/may-leetcoding-challenge/536/week-3-may-15th-may-21st/3332/)
+
+[Solution]()
