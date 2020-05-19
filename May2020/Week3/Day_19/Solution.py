7da79d804738d267a70c282d866b3275e86fdd7e
Dhanabal S <43275764+DhanabalShanmugam@users.noreply.github.com>
2020-05-19 14:56:53 +0530

    Update Solution.py

--- May2020/Week3/Day_19/Solution.py
+++ May2020/Week3/Day_19/Solution.py
@@ -5,13 +5,13 @@ class StockSpanner:
         self.spanCount = [0]
 
     def next(self, price: int) -> int:
-        spanCount = 1
+        span = 1
         while(self.stockList and self.stockList[-1]<=price):
-            spanCount += self.spanCount.pop()
+            span += self.spanCount.pop()
             self.stockList.pop()
         self.stockList.append(price)
-        self.spanCount.append(spanCount)
-        return spanCount
+        self.spanCount.append(span)
+        return span
         
 Time Complexity: O(Q).  O(Q), where QQ is the number of calls to StockSpanner.next. 
                         In total, there are Q pushes to the stack, and at most Q pops.
