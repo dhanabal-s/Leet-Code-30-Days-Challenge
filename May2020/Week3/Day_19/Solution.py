d9cb43c1748ff09ea6adbdb8bbf4d0e6d89ebecc
Dhanabal S <43275764+DhanabalShanmugam@users.noreply.github.com>
2020-05-19 14:54:04 +0530

    Create Solution.py

---
+++ May2020/Week3/Day_19/Solution.py
@@ -0,0 +1,19 @@
+class StockSpanner:
+
+    def __init__(self):
+        self.stockList = [0]
+        self.spanCount = [0]
+
+    def next(self, price: int) -> int:
+        spanCount = 1
+        while(self.stockList and self.stockList[-1]<=price):
+            spanCount += self.spanCount.pop()
+            self.stockList.pop()
+        self.stockList.append(price)
+        self.spanCount.append(spanCount)
+        return spanCount
+        
+Time Complexity: O(Q).  O(Q), where QQ is the number of calls to StockSpanner.next. In total, there are QQ pushes to the stack, and at most QQ pops.
+
+Space Complexity: O(Q).
+  
