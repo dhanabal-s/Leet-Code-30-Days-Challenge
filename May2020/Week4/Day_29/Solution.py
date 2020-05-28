06e75bc82e8483bf503552e322e527278b097b1c
Dhanabal S <43275764+DhanabalShanmugam@users.noreply.github.com>
2020-05-28 20:26:50 +0530

    Create Solution.py

---
+++ May2020/Week4/Day_29/Solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countBits(self, num: int) -> List[int]:
+        bitArr = [0]
+        
+        for val in range(1,num+1):
+            bitCount = bitArr[val//2] + val%2
+            bitArr.append(bitCount)
+        return bitArr
+        
+Time Complexity : O(N)
+
+Space Complexity : O(N)
+
