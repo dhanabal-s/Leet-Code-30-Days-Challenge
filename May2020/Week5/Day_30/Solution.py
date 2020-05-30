46e823fc761c44f843e1ae788b33b11b3466ebe3
Dhanabal S <43275764+DhanabalShanmugam@users.noreply.github.com>
2020-05-30 17:44:14 +0530

    Create Solution.py

---
+++ May2020/Week5/Day_30/Solution.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def kClosest(self, points, K):
+        points.sort(key = lambda P: P[0]**2 + P[1]**2)
+        return points[:K]
+        
+Time Complexity : O(NlogN)
+
+Space Complexity: O(1)
+
+
