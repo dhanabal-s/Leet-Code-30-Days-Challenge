961e2ed7d86b34c70aa555c7763bba93b2ea6bca
Dhanabal S <43275764+DhanabalShanmugam@users.noreply.github.com>
2020-05-30 18:39:34 +0530

    Create Solution.py

---
+++ May2020/Week4/Day_25/Solution.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxUncrossedLines(self, A: List[int], B: List[int]) -> int:
+
+        A = [ -1 ] + A
+        B = [ -1 ] + B
+
+        h, w = len(A), len(B)
+        dp_table = [ [ 0 for _ in range(w) ] for _ in range(h) ]
+        
+                
+        for y in range(1, h):
+            for x in range(1, w):
+                
+                if A[y] == B[x]:
+                    dp_table[y][x] = dp_table[y-1][x-1] + 1
+                    
+                else:
+                    dp_table[y][x] = max( dp_table[y][x-1], dp_table[y-1][x] )
+
+                    
+        return dp_table[-1][-1]
+
+
+Time Complexity : O(M*N)
+
+
+Space Complexity : O(M*N)
+
+
