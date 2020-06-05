b2bbe6a238ba28565506da8186781a6b354545cc
Dhanabal S <43275764+DhanabalShanmugam@users.noreply.github.com>
2020-06-05 10:27:29 +0530

    Create Solution.py

---
+++ Jun2020/Week1/Day5/Solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        left,right = 0,len(s)-1
+        while(left<right):
+            s[left],s[right]=s[right],s[left]
+            left,right = left+1,right-1
+        return s
+        
+
+Time Complexity : O(N/2)
+Space Complexity : O(1)
+
+
